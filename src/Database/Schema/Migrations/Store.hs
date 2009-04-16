cb52f3d2da9059bb63e0aeb17f71d609a3850ab3
drcygnus <drcygnus@gmail.com>
2009-04-15 22:03:14 -0700

    Skeletal filesystem store code
    Ignore-this: c69723bb5967f4cd8874223593d31114
    
    darcs-hash:20090416050314-e018d-e991f846d109e17bb34271920a8541bf1fae7d83.gz

--- src/Database/Schema/Migrations/Store.hs
+++ src/Database/Schema/Migrations/Store.hs
@@ -3,10 +3,23 @@ module Database.Schema.Migrations.Store
     )
 where
 
-import Database.Schema.Migrations.Migration ( Migration )
+import qualified Data.Map as Map
+import Database.Schema.Migrations.Migration
+    ( Migration
+    , MigrationID
+    )
 
 class MigrationStore a where
     createMigration :: a -> Migration -> IO ()
     saveMigration :: a -> Migration -> IO ()
     loadMigration :: a -> Int -> IO (Maybe Migration)
     getMigrations :: a -> IO [Migration]
+
+data FilesystemStore = FSStore { storePath :: FilePath
+                               , migrationMap :: Map.Map MigrationID Migration }
+
+newFilesystemStore :: FilePath -> IO FilesystemStore
+newFilesystemStore path = do
+  let migrations = Map.empty
+  return $ FSStore { storePath = path
+                   , migrationMap = migrations }
