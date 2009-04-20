79a2a583634c39cd15fd41614b85ca4bd89eaa09
drcygnus <drcygnus@gmail.com>
2009-04-19 17:28:22 -0700

    Clean up Store module
    Ignore-this: 5fde1b989279579c08b4c17f5b51d103
    
    darcs-hash:20090420002822-e018d-13f743a0a06a1e50aaba8269b4a1cb84d02106e8.gz

--- src/Database/Schema/Migrations/Store.hs
+++ src/Database/Schema/Migrations/Store.hs
@@ -3,10 +3,8 @@ module Database.Schema.Migrations.Store
     )
 where
 
-import qualified Data.Map as Map
 import Database.Schema.Migrations.Migration
     ( Migration
-    , MigrationID
     )
 
 class MigrationStore a where
@@ -14,12 +12,3 @@ class MigrationStore a where
     saveMigration :: a -> Migration -> IO ()
     loadMigration :: a -> Int -> IO (Maybe Migration)
     getMigrations :: a -> IO [Migration]
-
-data FilesystemStore = FSStore { storePath :: FilePath
-                               , migrationMap :: Map.Map MigrationID Migration }
-
-newFilesystemStore :: FilePath -> IO FilesystemStore
-newFilesystemStore path = do
-  let migrations = Map.empty
-  return $ FSStore { storePath = path
-                   , migrationMap = migrations }
