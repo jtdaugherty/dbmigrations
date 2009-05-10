ec2b2d6a024d6c296f1332eadca98cd7f7a4f72b
drcygnus <drcygnus@gmail.com>
2009-05-10 08:32:25 -0700

    Use fgl to represent dependency graph
    Ignore-this: 1d49103f0c4c99982adbbfe9b732d557
    
    darcs-hash:20090510153225-e018d-a983f094ffd21ae5ec35954d7bea7bfbfca157a2.gz

--- src/Database/Schema/Migrations/Dependencies.hs
+++ src/Database/Schema/Migrations/Dependencies.hs
@@ -1,27 +1,38 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 module Database.Schema.Migrations.Dependencies
     ( Dependable(..)
-    , DependencyInfo(..)
-    , depInfo
+    , DependencyGraph
+    , mkDepGraph
     )
 where
 
-import qualified Data.Map as Map
+import Data.Maybe ( fromJust )
+
+import Data.Graph.Inductive.Graph ( Graph(..), nodes, edges )
+import Data.Graph.Inductive.PatriciaTree ( Gr )
 
 class (Eq a, Ord a) => Dependable a where
     -- |The identifiers of the objects on which a depends.
     depsOf :: a -> [String]
     depId :: a -> String
 
-data DependencyInfo a = DepInfo { forwardDeps :: Map.Map String [String]
-                                , reverseDeps :: Map.Map String [String]
-                                , contents :: Map.Map String a
-                                }
-                      deriving (Show, Eq)
+type DependencyGraph = Gr String String
 
-depInfo :: (Dependable a) => [a] -> DependencyInfo a
-depInfo objects = DepInfo fd rd c
+instance Eq DependencyGraph where
+    g1 == g2 = (nodes g1 == nodes g2 &&
+                edges g1 == edges g2)
+
+instance Show DependencyGraph where
+    show g = "(" ++ (show $ nodes g) ++ ", " ++ (show $ edges g) ++ ")"
+
+mkDepGraph :: (Dependable a) => [a] -> DependencyGraph
+mkDepGraph objects = mkGraph n e
     where
-      c = Map.fromList [ (depId obj, obj) | obj <- objects ]
-      fd = Map.fromList [ (depId obj, depsOf obj) | obj <- objects ]
-      rd = Map.fromList [ (depId obj, rDeps obj fd) | obj <- objects ]
-      rDeps obj deps = [ k | (k, vs) <- Map.toList deps, depId obj `elem` vs ]
+      n = [ (fromJust $ lookup o ids, depId o) | o <- objects ]
+      e = [ ( fromJust $ lookup o ids
+            , fromJust $ lookup d ids
+            , depId o ++ " -> " ++ depId d) | o <- objects, d <- depsOf' o ]
+      depsOf' o = map (\i -> fromJust $ lookup i objMap) $ depsOf o
+
+      objMap = map (\o -> (depId o, o)) objects
+      ids = zip objects [1..]
--- test/DependencyTest.hs
+++ test/DependencyTest.hs
@@ -4,12 +4,12 @@ module DependencyTest
 where
 
 import Test.HUnit
-import qualified Data.Map as Map
+import Data.Graph.Inductive.Graph ( Graph(..) )
 
 import Database.Schema.Migrations.Dependencies
 
 tests :: [Test]
-tests = depInfoTests
+tests = depGraphTests
 
 data TestDependable = TD { tdId :: String
                          , tdDeps :: [String]
@@ -20,26 +20,23 @@ instance Dependable TestDependable where
     depId = tdId
     depsOf = tdDeps
 
-type DepInfoTestCase = ([TestDependable], DependencyInfo TestDependable)
+type DepGraphTestCase = ([TestDependable], DependencyGraph)
 
-depInfoTestCases :: [DepInfoTestCase]
-depInfoTestCases = [ ( []
-                     , DepInfo (Map.fromList []) (Map.fromList []) (Map.fromList [])
-                     )
-                   , ( [first, second]
-                     , DepInfo (Map.fromList [("first", []), ("second", ["first"])])
-                                   (Map.fromList [("second", []), ("first", ["second"])])
-                                   (Map.fromList [ ("first", first)
-                                                 , ("second", second)
-                                                 ]
-                                   )
-                     )
-                   ]
+depGraphTestCases :: [DepGraphTestCase]
+depGraphTestCases = [ ( []
+                     , empty
+                      )
+                    , ( [first, second]
+                      , mkGraph [(1, "first"), (2, "second")]
+                                   [(2, 1, "first -> second")]
+                      )
+                    ]
     where
       first = TD "first" []
       second = TD "second" ["first"]
-depInfoTests :: [Test]
-depInfoTests = map mkDepInfoTest depInfoTestCases
 
-mkDepInfoTest :: DepInfoTestCase -> Test
-mkDepInfoTest (input, expected) = expected ~=? depInfo input
+depGraphTests :: [Test]
+depGraphTests = map mkDepGraphTest depGraphTestCases
+
+mkDepGraphTest :: DepGraphTestCase -> Test
+mkDepGraphTest (input, expected) = expected ~=? mkDepGraph input
