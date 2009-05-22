efc0a287940c66b0cd1fd8475def2b16cac5ad25
drcygnus <drcygnus@gmail.com>
2009-05-21 22:22:10 -0700

    Use mId, not depId, in missingMigrations
    Ignore-this: 52f9773354e2f5690c460d2403ba5269
    
    darcs-hash:20090522052210-e018d-6eb8e88c7eea5a5fce937b2354bb710d48ddadcd.gz

--- src/Database/Schema/Migrations.hs
+++ src/Database/Schema/Migrations.hs
@@ -7,12 +7,12 @@ import qualified Data.Set as Set
 
 import qualified Database.Schema.Migrations.Store as MS
 import qualified Database.Schema.Migrations.Backend as B
-import Database.Schema.Migrations.Dependencies
+import Database.Schema.Migrations.Migration ( mId )
 
 missingMigrations :: (B.Backend b m, MS.MigrationStore s m) => b -> s -> m [String]
 missingMigrations backend store = do
   sm <- MS.getMigrations store
-  let storeMigrations = map depId sm
+  let storeMigrations = map mId sm
   backendMigrations <- B.getMigrations backend
 
   return $ Set.toList $ Set.difference
