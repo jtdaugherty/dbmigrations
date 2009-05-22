e4c762a515f362525e3febd685ae40476e868b53
drcygnus <drcygnus@gmail.com>
2009-05-21 23:05:50 -0700

    Add getMigration to MigrationStore class
    Ignore-this: 67cc9c75f171b65fe08073b33384f077
    
    darcs-hash:20090522060550-e018d-bd6e9b5c4805d2f1eb0e98726a01ad0add689920.gz

--- src/Database/Schema/Migrations/Store.hs
+++ src/Database/Schema/Migrations/Store.hs
@@ -4,10 +4,18 @@ module Database.Schema.Migrations.Store
     )
 where
 
+import Data.Maybe ( listToMaybe )
+
 import Database.Schema.Migrations.Migration
-    ( Migration
+    ( Migration(..)
     )
 
 class (Monad m) => MigrationStore s m where
     getMigrations :: s -> m [Migration]
+
+    getMigration :: s -> String -> m (Maybe Migration)
+    getMigration s theId = do
+      migrations <- getMigrations s
+      return $ listToMaybe [ e | e <- migrations, mId e == theId ]
+
     saveMigration :: s -> Migration -> m ()
