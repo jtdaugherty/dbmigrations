9c0fbbd06f256a78918dde82c6c677fa937a591f
drcygnus <drcygnus@gmail.com>
2009-05-22 19:13:18 -0700

    InitFS: add support for creating a new migration
    Ignore-this: eeb49b98d70ba55a76e0dbd143c5a57a
    
    darcs-hash:20090523021318-e018d-5d5f5483f2c52289873da4b3423680d5c1285ad2.gz

--- src/InitFS.hs
+++ src/InitFS.hs
@@ -5,6 +5,7 @@ where
 
 import System.Environment ( getArgs )
 import System.Exit ( exitWith, ExitCode(..) )
+import System.FilePath ( (</>) )
 
 import Control.Exception ( bracket )
 
@@ -14,8 +15,9 @@ import Database.HDBC.Sqlite3 ( connectSqlite3, Connection )
 import Database.HDBC ( IConnection(commit, disconnect) )
 
 import Database.Schema.Migrations.Filesystem
+import Database.Schema.Migrations.Migration ( Migration(..), newMigration )
 import Database.Schema.Migrations.Backend ( Backend, getBootstrapMigration, applyMigration )
-import Database.Schema.Migrations.Store ( MigrationStore, saveMigration, loadMigrations )
+import Database.Schema.Migrations.Store ( MigrationStore(..), loadMigrations )
 import Database.Schema.Migrations.Backend.Sqlite()
 
 initStore :: (Backend b IO) => b -> FilesystemStore -> IO ()
@@ -25,7 +27,7 @@ initStore backend store = do
 
 usage :: IO ()
 usage = do
-  putStrLn "Usage: initstore-fs <init> [...]"
+  putStrLn "Usage: initstore-fs <init|new|apply> [...]"
 
 withConnection :: FilePath -> (Connection -> IO a) -> IO a
 withConnection dbPath act = bracket (connectSqlite3 dbPath) disconnect act
@@ -40,6 +42,25 @@ main = do
              store = FSStore { storePath = fsPath }
          withConnection dbPath $ \conn ->
              initStore conn store
+
+    "new" -> do
+         let [fsPath, migrationId] = args
+             store = FSStore { storePath = fsPath }
+             fullPath = storePath store </> migrationId
+         available <- getMigrations store
+         case migrationId `elem` available of
+           True -> do
+                 putStrLn $ "Migration already exists: " ++ (show fullPath)
+                 exitWith (ExitFailure 1)
+           False -> do
+                 new <- newMigration migrationId
+                 -- Set some instructive defaults.
+                 let newWithDefaults = new { mDesc = Just "(Description here.)"
+                                           , mApply = "(Apply SQL here.)"
+                                           , mRevert = Just "(Revert SQL here.)"
+                                           }
+                 saveMigration store newWithDefaults
+                 putStrLn $ "New migration generated at " ++ (show fullPath)
 
     "apply" -> do
          let [fsPath, dbPath, migrationId] = args
