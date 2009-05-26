6f37a8fcf66a89895ebff867034e24d2a66cee5e
drcygnus <drcygnus@gmail.com>
2009-05-25 22:11:52 -0700

    InitFS: add support for applying all required dependencies in the correct order
    Ignore-this: 182b2e111c1642d753c752a32932d27a
    
    darcs-hash:20090526051152-e018d-7fd62b2e6b1c9ca5afd923588484dd3c60bffbb6.gz

--- src/InitFS.hs
+++ src/InitFS.hs
@@ -4,26 +4,41 @@ module Main
 where
 
 import System.Environment ( getArgs )
-import System.Exit ( exitWith, ExitCode(..) )
+import System.Exit ( exitWith, ExitCode(..), exitSuccess )
 import System.FilePath ( (</>) )
 
 import Control.Exception ( bracket )
 
 import qualified Data.Map as Map
+import Data.Maybe ( catMaybes )
+import Control.Monad ( when )
+import Data.List ( intercalate )
 
 import Database.HDBC.Sqlite3 ( connectSqlite3, Connection )
 import Database.HDBC ( IConnection(commit, disconnect) )
 
+import Database.Schema.Migrations ( missingMigrations )
 import Database.Schema.Migrations.Filesystem
+import Database.Schema.Migrations.Dependencies ( dependencies )
 import Database.Schema.Migrations.Migration ( Migration(..), newMigration )
-import Database.Schema.Migrations.Backend ( Backend, getBootstrapMigration, applyMigration )
-import Database.Schema.Migrations.Store ( MigrationStore(..), loadMigrations )
+import Database.Schema.Migrations.Backend ( Backend, getBootstrapMigration, isBootstrapped, applyMigration )
+import Database.Schema.Migrations.Store ( MigrationStore(..), loadMigrations, depGraphFromStore )
 import Database.Schema.Migrations.Backend.Sqlite()
 
 initStore :: (Backend b IO) => b -> FilesystemStore -> IO ()
 initStore backend store = do
   getBootstrapMigration backend >>= saveMigration store
   putStrLn $ "Filesystem store initialized at " ++ (storePath store)
+
+ensureBootstrappedBackend :: (Backend b IO) => b -> IO ()
+ensureBootstrappedBackend backend = do
+  bsStatus <- isBootstrapped backend
+  case bsStatus of
+    True -> return ()
+    False -> do
+      putStrLn "Database bootstrapping required; installing..."
+      getBootstrapMigration backend >>= applyMigration backend
+      putStrLn "Done."
 
 usage :: IO ()
 usage = do
@@ -69,14 +84,51 @@ main = do
          let theMigration = Map.lookup migrationId mapping
 
          withConnection dbPath $ \conn -> do
+                 ensureBootstrappedBackend conn >> commit conn
                  case theMigration of
                    Nothing -> do
                      putStrLn $ "No such migration: " ++ migrationId
                      exitWith (ExitFailure 1)
                    Just m -> do
-                     applyMigration conn m
+                     depGraph <- depGraphFromStore store
+                     graph <- case depGraph of
+                                Left e -> do
+                                     putStrLn $ "Error analyzing store: " ++ (show e)
+                                     exitWith (ExitFailure 1)
+                                Right g -> return g
+
+                     -- the list of migrations that need to be
+                     -- installed, including the requested migration.
+                     let deps = (mId m):(dependencies graph $ mId m)
+
+                     -- the list of available migrations that are not installed.
+                     allMissing <- missingMigrations conn mapping
+
+                     -- only the ones that are missing AND in deps are
+                     -- the ones we want to install.  Use deps as the
+                     -- source to maintain the correct installation
+                     -- order.
+                     let toInstall = reverse [ e | e <- deps, e `elem` allMissing ]
+
+                     loadedMigrations <- mapM (loadMigration store) toInstall
+                     let migrations = catMaybes loadedMigrations
+                     when (length migrations == 0) $ do
+                                            putStrLn $ "Nothing to do; " ++
+                                                         (mId m) ++
+                                                         " already installed."
+                                            exitSuccess
+
+                     putStrLn "About to apply migrations:"
+                     putStrLn $ intercalate "\n" (map mId migrations)
+
+                     mapM_ applyIt migrations
                      commit conn
-                     putStrLn $ "Successfully applied migration: " ++ migrationId
+                     putStrLn $ "Successfully applied migrations."
+                         where
+                           applyIt it = do
+                                     putStr $ "Applying: " ++ (mId it) ++ "... "
+                                     applyMigration conn it
+                                     putStrLn "done."
 
     _ -> do
          putStrLn $ "Unrecognized command: " ++ command
