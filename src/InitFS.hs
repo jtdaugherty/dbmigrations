336fcf13c94906ad6cee0bc92c4e256a13bec964
drcygnus <drcygnus@gmail.com>
2009-05-28 19:38:54 -0700

    InitFS: refactor functionality into commands, generate usage output from registered commands
    Ignore-this: 75deabb29b0211106868dc7be36d7595
    
    darcs-hash:20090529023854-e018d-1858fea2693f7e921e36f0942964e9c0eed0812c.gz

--- src/InitFS.hs
+++ src/InitFS.hs
@@ -9,8 +9,10 @@ import System.FilePath ( (</>) )
 
 import Control.Exception ( bracket )
 
+import Data.Maybe ( listToMaybe )
+import Data.List ( intercalate )
 import qualified Data.Map as Map
-import Control.Monad ( when )
+import Control.Monad ( when, forM_ )
 
 import Database.HDBC.Sqlite3 ( connectSqlite3, Connection )
 import Database.HDBC ( IConnection(commit, disconnect) )
@@ -26,79 +28,118 @@ import Database.Schema.Migrations.Backend ( Backend, getBootstrapMigration, appl
 import Database.Schema.Migrations.Store ( MigrationStore(..), loadMigrations )
 import Database.Schema.Migrations.Backend.Sqlite()
 
-initStore :: (Backend b IO) => b -> FilesystemStore -> IO ()
-initStore backend store = do
-  getBootstrapMigration backend >>= saveMigration store
-  putStrLn $ "Filesystem store initialized at " ++ (storePath store)
+-- A command has a name, a number of required arguments' labels, a
+-- number of optional arguments' labels, and an action to invoke.
+data Command = Command { cName :: String
+                       , cRequired :: [String]
+                       , cOptional :: [String]
+                       , cHandler :: CommandHandler
+                       }
+-- (required arguments, optional arguments) -> IO ()
+type CommandHandler = ([String], [String]) -> IO ()
 
-usage :: IO ()
-usage = do
-  putStrLn "Usage: initstore-fs <init|new|apply> [...]"
+commands :: [Command]
+commands = [ Command "init" ["store_path", "db_path"] [] initStore
+           , Command "new" ["store_path", "db_path", "migration_name"] [] new
+           , Command "apply" ["store_path", "db_path", "migration_name"] [] apply
+           ]
 
 withConnection :: FilePath -> (Connection -> IO a) -> IO a
 withConnection dbPath act = bracket (connectSqlite3 dbPath) disconnect act
 
+initStore :: CommandHandler
+initStore (required, _) = do
+  let [fsPath, dbPath] = required
+      store = FSStore { storePath = fsPath }
+  withConnection dbPath $ \conn -> do
+      getBootstrapMigration conn >>= saveMigration store
+      putStrLn $ "Filesystem store initialized at " ++ (storePath store)
+
+new :: CommandHandler
+new (required, _) = do
+  let [fsPath, migrationId] = required
+      store = FSStore { storePath = fsPath }
+      fullPath = storePath store </> migrationId
+  status <- createNewMigration store migrationId
+  case status of
+    Left e -> putStrLn e >> (exitWith (ExitFailure 1))
+    Right _ -> putStrLn $ "Migration created successfully: " ++ (show fullPath)
+
+apply :: CommandHandler
+apply (required, _) = do
+  let [fsPath, dbPath, migrationId] = required
+      store = FSStore { storePath = fsPath }
+  mapping <- loadMigrations store
+
+  withConnection dbPath $ \conn ->
+      do
+        ensureBootstrappedBackend conn >> commit conn
+
+        -- Look up the migration
+        let theMigration = Map.lookup migrationId mapping
+        m <- case theMigration of
+               Nothing -> do
+                        putStrLn $ "No such migration: " ++ migrationId
+                        exitWith (ExitFailure 1)
+               Just m' -> return m'
+
+        -- Get the list of migrations to apply
+        toApply' <- migrationsToApply mapping conn m
+        toApply <- case toApply' of
+                     Left e -> do
+                        putStrLn $ "Error: " ++ e
+                        exitWith (ExitFailure 1)
+                     Right ms -> return ms
+
+        -- Apply them
+        when (null toApply) $ do
+                        putStrLn $ "Nothing to do; " ++
+                                     (mId m) ++
+                                     " already installed."
+                        exitSuccess
+
+        mapM_ (applyIt conn) toApply
+        commit conn
+        putStrLn $ "Successfully applied migrations."
+            where
+              applyIt conn it = do
+                        putStr $ "Applying: " ++ (mId it) ++ "... "
+                        applyMigration conn it
+                        putStrLn "done."
+
+usageString :: Command -> String
+usageString command = intercalate " " ((cName command):requiredArgs ++ optionalArgs)
+    where
+      requiredArgs = map (\s -> "<" ++ s ++ ">") $ cRequired command
+      optionalArgs = map (\s -> "[" ++ s ++ "]") $ cOptional command
+
+usage :: IO a
+usage = do
+  putStrLn $ "Usage: initstore-fs <command> [args]"
+  putStrLn "Commands:"
+  forM_ commands $ \command -> do
+          putStrLn $ "  " ++ usageString command
+  exitWith (ExitFailure 1)
+
+usageSpecific :: Command -> IO a
+usageSpecific command = do
+  putStrLn $ "Usage: initstore-fs " ++ usageString command
+  exitWith (ExitFailure 1)
+
+findCommand :: String -> Maybe Command
+findCommand name = listToMaybe [ c | c <- commands, cName c == name ]
+
 main :: IO ()
 main = do
-  (command:args) <- getArgs
+  allArgs <- getArgs
+  when (null allArgs) usage
 
-  case command of
-    "init" -> do
-         let [fsPath, dbPath] = args
-             store = FSStore { storePath = fsPath }
-         withConnection dbPath $ \conn ->
-             initStore conn store
+  let (commandName:args) = allArgs
 
-    "new" -> do
-         let [fsPath, migrationId] = args
-             store = FSStore { storePath = fsPath }
-             fullPath = storePath store </> migrationId
-         status <- createNewMigration store migrationId
-         case status of
-           Left e -> putStrLn e >> (exitWith (ExitFailure 1))
-           Right _ -> putStrLn $ "Migration created successfully: " ++ (show fullPath)
+  command <- case findCommand commandName of
+               Nothing -> usage
+               Just c -> return c
 
-    "apply" -> do
-         let [fsPath, dbPath, migrationId] = args
-             store = FSStore { storePath = fsPath }
-         mapping <- loadMigrations store
-
-         withConnection dbPath $ \conn -> do
-                 ensureBootstrappedBackend conn >> commit conn
-
-                 -- Look up the migration
-                 let theMigration = Map.lookup migrationId mapping
-                 m <- case theMigration of
-                        Nothing -> do
-                          putStrLn $ "No such migration: " ++ migrationId
-                          exitWith (ExitFailure 1)
-                        Just m' -> return m'
-
-                 -- Get the list of migrations to apply
-                 toApply' <- migrationsToApply mapping conn m
-                 toApply <- case toApply' of
-                              Left e -> do
-                                putStrLn $ "Error: " ++ e
-                                exitWith (ExitFailure 1)
-                              Right ms -> return ms
-
-                 -- Apply them
-                 when (length toApply == 0) $ do
-                                            putStrLn $ "Nothing to do; " ++
-                                                         (mId m) ++
-                                                         " already installed."
-                                            exitSuccess
-
-                 mapM_ (applyIt conn) toApply
-                 commit conn
-                 putStrLn $ "Successfully applied migrations."
-                     where
-                       applyIt conn it = do
-                         putStr $ "Applying: " ++ (mId it) ++ "... "
-                         applyMigration conn it
-                         putStrLn "done."
-
-    _ -> do
-         putStrLn $ "Unrecognized command: " ++ command
-         usage
-         exitWith (ExitFailure 1)
+  if (length args) < (length $ cRequired command) then
+      usageSpecific command else
+      cHandler command $ splitAt (length $ cRequired command) args
