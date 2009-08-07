a3a3ae3dd8c53fca4576adb27121b7e57042a841
drcygnus <drcygnus@gmail.com>
2009-08-07 09:18:41 -0700

    Add docstring for Backend type class
    Ignore-this: 1acb52e991be1ffa616ba439bab0d2d2
    
    darcs-hash:20090807161841-e018d-2b366968219bbf3e51597f001f2152486218b348.gz

--- src/Database/Schema/Migrations/Backend.hs
+++ src/Database/Schema/Migrations/Backend.hs
@@ -7,6 +7,12 @@ where
 import Database.Schema.Migrations.Migration
     ( Migration(..) )
 
+-- |A Backend represents a database engine backend such as MySQL or
+-- SQLite.  A Backend supplies relatively low-level functions for
+-- inspecting the backend's state, applying migrations, and reverting
+-- migrations.  A Backend also supplies the migration necessary to
+-- "bootstrap" a backend so that it can track which migrations are
+-- installed.
 class (Monad m) => Backend b m where
     -- |The migration necessary to bootstrap a database with this
     -- connection interface.  This might differ slightly from one
