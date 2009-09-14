1a52af49adeb6ac8bf28c3390188bb77c58785bc
drcygnus <drcygnus@gmail.com>
2009-09-14 09:07:54 -0700

    StoreTest: add tests for validateMigrationMap
    Ignore-this: a604db63d5ffdad63e454618db39bcb1
    
    darcs-hash:20090914160754-e018d-c9b6244441976b3391170d3171af16d8c6531a87.gz

--- test/StoreTest.hs
+++ test/StoreTest.hs
@@ -11,11 +11,16 @@ import Database.Schema.Migrations.Store
 
 tests :: [Test]
 tests = validateSingleMigrationTests
+        ++ validateMigrationMapTests
 
 type ValidateSingleTestCase = ( MigrationMap
                               , Migration
                               , [MapValidationError]
                               )
+
+type ValidateMigrationMapTestCase = ( MigrationMap
+                                    , [MapValidationError]
+                                    )
 
 emptyMap :: MigrationMap
 emptyMap = Map.fromList []
@@ -67,3 +72,47 @@ validateSingleMigrationTests =
         where
           mkValidateSingleTest (mmap, m, errs) =
               errs ~=? validateSingleMigration mmap m
+
+m1 :: Migration
+m1 = noDeps { mId = "m1"
+            , mDeps = [] }
+
+m2 :: Migration
+m2 = noDeps { mId = "m2"
+            , mDeps = ["m1"] }
+
+m3 :: Migration
+m3 = noDeps { mId = "m3"
+            , mDeps = ["nonexistent"] }
+
+m4 :: Migration
+m4 = noDeps { mId = "m4"
+            , mDeps = ["one", "two"] }
+
+map1 :: MigrationMap
+map1 = Map.fromList [ ("m1", m1)
+                    , ("m2", m2)
+                    ]
+
+map2 :: MigrationMap
+map2 = Map.fromList [ ("m3", m3)
+                    ]
+
+map3 :: MigrationMap
+map3 = Map.fromList [ ("m4", m4)
+                    ]
+
+validateMapTestCases :: [ValidateMigrationMapTestCase]
+validateMapTestCases = [ (emptyMap, [])
+                       , (map1, [])
+                       , (map2, [DependencyReferenceError (mId m3) "nonexistent"])
+                       , (map3, [ DependencyReferenceError (mId m4) "one"
+                                , DependencyReferenceError (mId m4) "two"])
+                       ]
+
+validateMigrationMapTests :: [Test]
+validateMigrationMapTests =
+    map mkValidateMapTest validateMapTestCases
+        where
+          mkValidateMapTest (mmap, errs) =
+              errs ~=? validateMigrationMap mmap
