8c2e60ec3748234c5530732ae9e650bc6f268979
yac <yac@blesmrt.net>
2015-02-21 18:08:42 +0100

    Split main into Moo.Main
    So library users can easily write a moo wrapper that will fill in the
    needed configuration from the application config and otherwise act like
    the moo executable itself.

--- src/Moo.hs
+++ src/Moo.hs
@@ -1,50 +1,15 @@
 module Main
-    ( main )
+    ( main
+    )
 where
 
 import  Control.Monad (liftM)
-import  Control.Monad.Reader (forM_, runReaderT, when)
 import  Data.Configurator
-import  Data.List (intercalate)
-import  Data.Maybe (fromMaybe)
-import  Database.HDBC (SqlError, catchSql, seErrorMsg)
 import  Prelude  hiding (lookup)
-import  System.Environment (getArgs, getEnvironment, getProgName)
-import  System.Exit (ExitCode (ExitFailure), exitWith)
+import  System.Environment (getArgs, getEnvironment)
 
-import  Database.Schema.Migrations.Filesystem (FilesystemStore (..))
-import  Database.Schema.Migrations.Store
-import  Moo.CommandInterface
 import  Moo.Core
-
-reportSqlError :: SqlError -> IO a
-reportSqlError e = do
-  putStrLn $ "\n" ++ "A database error occurred: " ++ seErrorMsg e
-  exitWith (ExitFailure 1)
-
-usage :: IO a
-usage = do
-  progName <- getProgName
-
-  putStrLn $ "Usage: " ++ progName ++ " <command> [args]"
-  putStrLn "Environment:"
-  putStrLn $ "  " ++ envDatabaseName ++ ": database connection string"
-  putStrLn $ "  " ++ envDatabaseType ++ ": database type, one of " ++
-           intercalate "," (map fst databaseTypes)
-  putStrLn $ "  " ++ envStoreName ++ ": path to migration store"
-  putStrLn "Commands:"
-  forM_ commands $ \command -> do
-          putStrLn $ "  " ++ usageString command
-          putStrLn $ "  " ++ _cDescription command
-          putStrLn ""
-
-  putStrLn commandOptionUsage
-  exitWith (ExitFailure 1)
-
-usageSpecific :: Command -> IO a
-usageSpecific command = do
-  putStrLn $ "Usage: initstore-fs " ++ usageString command
-  exitWith (ExitFailure 1)
+import  Moo.Main
 
 loadConfiguration :: Maybe FilePath -> IO Configuration
 loadConfiguration Nothing = liftM fromShellEnvironment getEnvironment
@@ -52,44 +17,7 @@ loadConfiguration (Just path) = fromConfigurator =<< load [Required path]
 
 main :: IO ()
 main = do
-  allArgs <- getArgs
-  when (null allArgs) usage
-  let (commandName:unprocessedArgs) = allArgs
-
-  command <- case findCommand commandName of
-               Nothing -> usage
-               Just c -> return c
-
-  (opts, required) <- getCommandArgs unprocessedArgs
-
-  let optionalConfigPath = _configFilePath opts
-
-  conf <- loadConfiguration optionalConfigPath
-  let mDbConnStr = _connectionString conf
-  let mDbType = _databaseType conf
-  let mStoreName = _migrationStorePath conf
-  let  storePathStr =
-         fromMaybe (error $ "Error: missing required environment variable " ++ envStoreName)
-                   mStoreName
-
-  let  store = FSStore { storePath = storePathStr }
-
-  if length required < length ( _cRequired command) then
-      usageSpecific command else
-      do
-        loadedStoreData <- loadMigrations store
-        case loadedStoreData of
-          Left es -> do
-            putStrLn "There were errors in the migration store:"
-            forM_ es $ \err -> putStrLn $ "  " ++ show err
-          Right storeData -> do
-            let st = AppState { _appOptions = opts
-                              , _appCommand = command
-                              , _appRequiredArgs = required
-                              , _appOptionalArgs = ["" :: String]
-                              , _appDatabaseConnStr = liftM DbConnDescriptor mDbConnStr
-                              , _appDatabaseType = mDbType
-                              , _appStore = FSStore storePathStr
-                              , _appStoreData = storeData
-                              }
-            runReaderT (_cHandler command storeData) st `catchSql` reportSqlError
+  args <- getArgs
+  (_, opts, _) <- procArgs args
+  conf <- loadConfiguration $ _configFilePath opts
+  mainWithConf args conf
---
+++ src/Moo/Main.hs
@@ -0,0 +1,99 @@
+module Moo.Main
+    ( mainWithConf
+    , Configuration (..)
+    , Args
+    , usage
+    , usageSpecific
+    , procArgs
+    )
+where
+
+import  Control.Monad (liftM)
+import  Control.Monad.Reader (forM_, runReaderT, when)
+import  Data.List (intercalate)
+import  Data.Maybe (fromMaybe)
+import  Database.HDBC (SqlError, catchSql, seErrorMsg)
+import  Prelude  hiding (lookup)
+import  System.Environment (getProgName)
+import  System.Exit (ExitCode (ExitFailure), exitWith)
+
+import  Database.Schema.Migrations.Filesystem (FilesystemStore (..))
+import  Database.Schema.Migrations.Store
+import  Moo.CommandInterface
+import  Moo.Core
+
+type Args = [String]
+
+usage :: IO a
+usage = do
+  progName <- getProgName
+
+  putStrLn $ "Usage: " ++ progName ++ " <command> [args]"
+  putStrLn "Environment:"
+  putStrLn $ "  " ++ envDatabaseName ++ ": database connection string"
+  putStrLn $ "  " ++ envDatabaseType ++ ": database type, one of " ++
+           intercalate "," (map fst databaseTypes)
+  putStrLn $ "  " ++ envStoreName ++ ": path to migration store"
+  putStrLn "Commands:"
+  forM_ commands $ \command -> do
+          putStrLn $ "  " ++ usageString command
+          putStrLn $ "  " ++ _cDescription command
+          putStrLn ""
+
+  putStrLn commandOptionUsage
+  exitWith (ExitFailure 1)
+
+usageSpecific :: Command -> IO a
+usageSpecific command = do
+  putStrLn $ "Usage: initstore-fs " ++ usageString command
+  exitWith (ExitFailure 1)
+
+procArgs :: Args -> IO (Command, CommandOptions, [String])
+procArgs args = do
+  when (null args) usage
+
+  command <- case findCommand $ head args of
+               Nothing -> usage
+               Just c -> return c
+
+  (opts, required) <- getCommandArgs $ tail args
+
+  return (command, opts, required)
+
+mainWithConf :: Args -> Configuration -> IO ()
+mainWithConf args conf = do
+  (command, opts, required) <- procArgs args
+
+  let mDbConnStr = _connectionString conf
+  let mDbType = _databaseType conf
+  let mStoreName = _migrationStorePath conf
+  let  storePathStr =
+         fromMaybe (error $ "Error: missing required environment variable " ++ envStoreName)
+                   mStoreName
+
+  let  store = FSStore { storePath = storePathStr }
+
+  if length required < length ( _cRequired command) then
+      usageSpecific command else
+      do
+        loadedStoreData <- loadMigrations store
+        case loadedStoreData of
+          Left es -> do
+            putStrLn "There were errors in the migration store:"
+            forM_ es $ \err -> putStrLn $ "  " ++ show err
+          Right storeData -> do
+            let st = AppState { _appOptions = opts
+                              , _appCommand = command
+                              , _appRequiredArgs = required
+                              , _appOptionalArgs = ["" :: String]
+                              , _appDatabaseConnStr = liftM DbConnDescriptor mDbConnStr
+                              , _appDatabaseType = mDbType
+                              , _appStore = FSStore storePathStr
+                              , _appStoreData = storeData
+                              }
+            runReaderT (_cHandler command storeData) st `catchSql` reportSqlError
+
+reportSqlError :: SqlError -> IO a
+reportSqlError e = do
+  putStrLn $ "\n" ++ "A database error occurred: " ++ seErrorMsg e
+  exitWith (ExitFailure 1)
