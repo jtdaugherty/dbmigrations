35602377622b8e17be36c8c0f49b6d37f11f3d2d
Jonathan Daugherty <cygnus@foobox.com>
2015-12-29 14:03:14 -0800

    Remove bit-rotten test

--- test/LinearMigrationsTest.hs
+++ test/LinearMigrationsTest.hs
@@ -14,7 +14,6 @@ import           Moo.Core
 
 tests :: IO [Test]
 tests = sequence [ addsMigration
-                 -- , doesNotSetTimestamp
                  , selectsLatestMigrationAsDep
                  , selectsOnlyLeavesAsDeps
                  , doesNotAddDependencyWhenLinearMigrationsAreDisabled
@@ -25,14 +24,6 @@ addsMigration = do
     state <- prepareState "first"
     mig <- addTestMigration state
     satisfies "Migration not added" mig isRight
-
--- What is this test case for? It'll never pass since the `addsMigration`
--- test case will generate a migration with a timestamp.
-doesNotSetTimestamp :: IO Test
-doesNotSetTimestamp = do
-    state <- prepareState "first"
-    Right mig <- addTestMigration state
-    satisfies "Timestamp is set" (mTimestamp mig) isNothing
 
 selectsLatestMigrationAsDep :: IO Test
 selectsLatestMigrationAsDep = do
