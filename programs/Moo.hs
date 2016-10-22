ab955622ac9501974b3c3aadb7a0fe9b094d9b9a
Jonathan Daugherty <cygnus@foobox.com>
2016-10-22 12:33:14 -0700

    Update placeholder moo error message

--- programs/Moo.hs
+++ programs/Moo.hs
@@ -10,9 +10,8 @@ main = do
   error $
     "This package (dbmigrations) does no longer contain the executable to \
     \create, apply or revert database migrations. Please install the specific \
-    \wrapper-package for your database, that is, dbmigrations-postgresql, \
+    \wrapper package for your database: dbmigrations-postgresql, \
     \dbmigrations-mysql, or dbmigrations-sqlite. These packages contain \
-    \database specific executables (moo-postgresql, moo-mysql and moo-sqlite \
-    \respectively) that replace the former moo executable from the \
+    \database-specific executables that replace the former moo executable from the \
     \dbmigrations package."
 
